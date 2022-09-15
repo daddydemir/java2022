764b857e6cedd8be7637543f114e5acdf5849730
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-15 15:05:56 +0300

    Data types added.

---
+++ src/com/company/DataTypes.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class DataTypes {
+
+    public static void main(String[] args){
+        byte sayi = 127;
+        int tamSayi = 12;
+        double ondalikliSayi = 12.5;
+
+        String sehir = "ANKARA";
+        char karakter = 'D';
+
+        boolean dogruMu = true;
+
+        if(dogruMu == false){
+            // code block
+        }
+    }
+}

d347ddf764de166119c234ce4339a6c705a5ec19
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-15 16:51:24 +0300

    Sesli harfler added.

---
+++ src/com/company/SesliHarfler.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class SesliHarfler {
+    public static void main(String[] args){
+
+        char harf = 'E';
+
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın sesli harf");
+                break;
+            default:
+                System.out.println("İnce seslli harf");
+        }
+    }
+}

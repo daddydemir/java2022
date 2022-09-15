b10b7dd54ab2201e7d59ec5c6d42731e1f5056e2
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-15 15:32:06 +0300

    while loop added.

--- src/com/company/LoopDemo.java
+++ src/com/company/LoopDemo.java
@@ -7,6 +7,13 @@ public class LoopDemo {
         for (int i=2;i<10;i+=2){
             System.out.println(i);
         }
-        System.out.println("Döngü Bitti");
+        System.out.println("For Döngüsü Bitti");
+
+        int i = 1;
+        while(i < 10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While Döngüsü Bitti");
     }
 }

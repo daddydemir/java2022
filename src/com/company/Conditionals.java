85c584802939a30efc728941fc96e1fd84401124
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-15 15:12:46 +0300

    Conditionals added.

---
+++ src/com/company/Conditionals.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Conditionals {
+
+    public static void  main(String[] args){
+        int sayi = 14;
+
+        if(sayi < 20){
+            System.out.println("Sayı 20' den küçüktür.");
+        }else if(sayi == 20){
+            System.out.println("Sayı 20' ye eşittir.");
+        }else{
+            System.out.println("Sayı 20' den büyüktür.");
+        }
+
+        if(sayi < 15){
+            System.out.println("Sayı 15' den küçüktür.");
+        }
+    }
+}

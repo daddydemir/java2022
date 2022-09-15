f5bccc262b0f06c329b58b5f82beb213582c1edd
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-15 15:29:08 +0300

    for loop added.

---
+++ src/com/company/LoopDemo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class LoopDemo {
+
+    public static void main(String[] args){
+
+        for (int i=2;i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
+    }
+}

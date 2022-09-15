a8b1b01d0ed6cfdfa018e506b8714c49bd77267a
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-15 14:54:45 +0300

    Variables added.

---
+++ src/com/company/Variables.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Variables {
+
+    public static void main(String[] args){
+        int sayi = 10;
+        String mesaj = "Öğrenci sayısı";
+        System.out.println(mesaj + sayi);
+    }
+}

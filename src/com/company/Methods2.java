1629507398fbec5165a46b2eb40409a0abb9e15f
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-22 23:38:56 +0300

    variable arguments added.

--- src/com/company/Methods2.java
+++ src/com/company/Methods2.java
@@ -7,6 +7,8 @@ public class Methods2 {
         String yeniMesaj = sehirVer();
         System.out.println(yeniMesaj);
         int sayi = topla(5,7);
+        int toplam = topla2(3,46,7,89,2);
+        System.out.println(toplam);
     }
 
     public static void ekle(){
@@ -23,6 +25,14 @@ public class Methods2 {
 
     public static int topla(int sayi1, int sayi2){
         return sayi1+sayi2;
+    }
+
+    public static int topla2(int... sayilar){
+        int toplam =0;
+        for(int sayi: sayilar){
+            toplam += sayi;
+        }
+        return toplam;
     }
 
     public static String sehirVer(){

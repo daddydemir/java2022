bbfe9f5a2e106ab77f2fb0642aea3c533f9b2054
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-23 11:19:19 +0300

    classes updated.

--- src/com/company/Classes.java
+++ src/com/company/Classes.java
@@ -1,17 +1,28 @@
 package com.company;
 
 public class Classes {
+    public static void main(String[] args) {
+        // referance type
+        CustomerManager customerManager = new CustomerManager();
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+    }
 }
 
 
-class CustomerManager{
-    public void Add(){
+class CustomerManager {
+    public void Add() {
         System.out.println("Müşteri eklendi.");
     }
-    public void Remove(){
+
+    public void Remove() {
         System.out.println("Müşteri silindi.");
     }
-    public void Update(){
+
+    public void Update() {
         System.out.println("Müşteri güncellendi.");
     }
 

dc7f8d50b222333dbdd224e8cb78bbfaadb68dd7
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-23 11:47:19 +0300

    encapsulation added.

--- src/com/company/ClassesWithAttributes.java
+++ src/com/company/ClassesWithAttributes.java
@@ -8,23 +8,34 @@ public class ClassesWithAttributes {
         product.description = "Asus";
         product.price = 5000;
         product.stockAmount = 3;
+        product.renk = "";
         System.out.println(product.name);
 
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
+/*
+        productManager.Add2(1,"","",2,200);
+        productManager.Add2(1,"","",2,200);
+        productManager.Add2(1,"","",2,200);
+        productManager.Add2(1,"","",2,200);
+  */
     }
 }
 
 class Product{
-    int id;
-    String name;
-    String description;
-    double price;
-    int stockAmount;
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stockAmount;
+    private String renk;
+    private String kod;
 }
 
 class ProductManager{
     public void Add(Product product){
         System.out.println("Ürin eklendi: " + product.name);
     }
+
+    public void Add2(int id, String name, String description, int stockAmount, double price, String yeniOzellik){}
 }

fdbb9318d68e9c41100e760b00f91d579392f588
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-09-23 11:27:59 +0300

    value and referance type added.

--- src/com/company/Classes.java
+++ src/com/company/Classes.java
@@ -9,6 +9,21 @@ public class Classes {
         customerManager.Add();
         customerManager.Remove();
         customerManager.Update();
+
+        // value type
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+        System.out.println(sayi2);
+
+        // referance type
+        int[] sayilar1 = {1,2,3};
+        int[] sayilar2 = {4,5,6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
+
     }
 }
 
